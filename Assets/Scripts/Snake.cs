zigurous/unity-snake-tutorial

Implement custom sprites with directional segments

Assets/Scripts/Snake.cs
@@ -10,13 +10,18 @@ public class Snake : MonoBehaviour
     /// <summary>
     /// The list of segments of the snake.
     /// </summary>
-    private List<Transform> _segments = new List<Transform>();
+    private List<SnakeSegment> _segments = new List<SnakeSegment>();
+
+    /// <summary>
+    /// The head snake segment.
+    /// </summary>
+    private SnakeSegment _head;
 
     /// <summary>
     /// The object that is cloned when creating a new segment to grow the snake.
     /// </summary>
     [Tooltip("The object that is cloned when creating a new segment to grow the snake.")]
-    public Transform segmentPrefab;
+    public SnakeSegment segmentPrefab;
 
     /// <summary>
     /// The number of segments the snake has initially.
@@ -24,11 +29,16 @@ public class Snake : MonoBehaviour
     [Tooltip("The number of segments the snake has initially.")]
     public int initialSize = 4;
 
-    /// <summary>
-    /// The direction the snake is moving.
-    /// </summary>
-    [HideInInspector]
-    public Vector2 direction = Vector2.right;
+    private void Awake()
+    {
+        _head = GetComponent<SnakeSegment>();
+
+        if (_head == null)
+        {
+            _head = this.gameObject.AddComponent<SnakeSegment>();
+            _head.hideFlags = HideFlags.HideInInspector;
+        }
+    }
 
     private void Start()
     {
@@ -38,50 +48,48 @@ public class Snake : MonoBehaviour
     private void Update()
     {
         // If moving horizontal, then only allow turning up or down
-        if (this.direction.x != 0.0f)
+        if (_head.direction.x != 0.0f)
         {
             // Set the direction based on the input key being pressed
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
-                this.direction = Vector2.up;
+                _head.SetDirection(Vector2.up, Vector2.zero);
             } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
-                this.direction = Vector2.down;
+                _head.SetDirection(Vector2.down, Vector2.zero);
             }
         }
         // If moving vertical, then only allow turning left or right
-        else if (this.direction.y != 0.0f)
+        else if (_head.direction.y != 0.0f)
         {
             // Set the direction based on the input key being pressed
             if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
-                this.direction = Vector2.right;
+                _head.SetDirection(Vector2.right, Vector2.zero);
             } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-                this.direction = Vector2.left;
+                _head.SetDirection(Vector2.left, Vector2.zero);
             }
         }
     }
 
     private void FixedUpdate()
     {
-        // Set each segment's position to be the same as the one it follows. We
-        // must do this in reverse order so the position is set to the previous
-        // position, otherwise they will all be stacked on top of each other.
+        // Have each segment follow the one in front of it. We must do this in
+        // reverse order so the position is set to the previous position,
+        // otherwise they will all be stacked on top of each other.
         for (int i = _segments.Count - 1; i > 0; i--) {
-            _segments[i].position = _segments[i - 1].position;
+            _segments[i].Follow(_segments[i - 1], i, _segments.Count);
         }
 
         // Increase the snake's position by one in the direction they are
         // moving. Round the position to ensure it stays aligned to the grid.
-        this.transform.position = new Vector3(
-            Mathf.Round(this.transform.position.x) + this.direction.x,
-            Mathf.Round(this.transform.position.y) + this.direction.y);
+        _head.transform.position = new Vector3(
+            Mathf.Round(_head.transform.position.x) + _head.direction.x,
+            Mathf.Round(_head.transform.position.y) + _head.direction.y);
     }
 
     public void Grow()
     {
-        // Create a new segment
-        Transform segment = Instantiate(this.segmentPrefab);
-
-        // Position the segment at the same spot as the last segment
-        segment.position = _segments[_segments.Count - 1].position;
+        // Create a new segment and have it follow the last segment
+        SnakeSegment segment = Instantiate(this.segmentPrefab);
+        segment.Follow(_segments[_segments.Count - 1], _segments.Count, _segments.Count + 1);
 
         // Add the segment to the end of the list
         _segments.Add(segment);
@@ -91,17 +99,17 @@ public class Snake : MonoBehaviour
     {
         // Set the initial direction of the snake, starting at the origin
         // (center of the grid)
-        this.direction = Vector2.right;
-        this.transform.position = Vector3.zero;
+        _head.SetDirection(Vector2.right, Vector2.zero);
+        _head.transform.position = Vector3.zero;
 
         // Start at 1 to skip destroying the head
         for (int i = 1; i < _segments.Count; i++) {
             Destroy(_segments[i].gameObject);
         }
 
-        // Clear the list then add the head (this) as the first segment
+        // Clear the list then add the head as the first segment
         _segments.Clear();
-        _segments.Add(this.transform);
+        _segments.Add(_head);
 
         // Grow the snake to the initial size -1 since the head was already
         // added

caoduylong2000/Snake

Assets/Scripts/Snake.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Snake : MonoBehaviour
 {
-    private List<Transform> segments = new List<Transform>();
-    public Transform segmentPrefab;
-    public Vector2 direction = Vector2.right;
+    private List<SnakeSegment> segments = new List<SnakeSegment>();
+    private SnakeSegment head;
+    public SnakeSegment segmentPrefab;
     public int initialSize = 4;
+
+    private void Awake()
+    {
+        head = GetComponent<SnakeSegment>();
+
+        if (head == null)
+        {
+            head = gameObject.AddComponent<SnakeSegment>();
+            head.hideFlags = HideFlags.HideInInspector;
+        }
+    }
 
     private void Start()
     {
@@ -17,62 +28,64 @@ public class Snake : MonoBehaviour
     private void Update()
     {
         // Only allow turning up or down while moving in the x-axis
-        if (direction.x != 0f)
+        if (head.direction.x != 0f)
         {
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
-                direction = Vector2.up;
+                head.SetDirection(Vector2.up, Vector2.zero);
             } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
-                direction = Vector2.down;
+                head.SetDirection(Vector2.down, Vector2.zero);
             }
         }
         // Only allow turning left or right while moving in the y-axis
-        else if (direction.y != 0f)
+        else if (head.direction.y != 0f)
         {
             if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
-                direction = Vector2.right;
+                head.SetDirection(Vector2.right, Vector2.zero);
             } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-                direction = Vector2.left;
+                head.SetDirection(Vector2.left, Vector2.zero);
             }
         }
     }
 
     private void FixedUpdate()
     {
-        // Set each segment's position to be the same as the one it follows. We
-        // must do this in reverse order so the position is set to the previous
-        // position, otherwise they will all be stacked on top of each other.
+        // Have each segment follow the one in front of it. We must do this in
+        // reverse order so the position is set to the previous position,
+        // otherwise they will all be stacked on top of each other.
         for (int i = segments.Count - 1; i > 0; i--) {
-            segments[i].position = segments[i - 1].position;
+            segments[i].Follow(segments[i - 1], i, segments.Count);
         }
 
         // Move the snake in the direction it is facing
         // Round the values to ensure it aligns to the grid
-        float x = Mathf.Round(transform.position.x) + direction.x;
-        float y = Mathf.Round(transform.position.y) + direction.y;
+        float x = Mathf.Round(head.transform.position.x) + head.direction.x;
+        float y = Mathf.Round(head.transform.position.y) + head.direction.y;
 
-        transform.position = new Vector2(x, y);
+        head.transform.position = new Vector2(x, y);
     }
 
     public void Grow()
     {
-        Transform segment = Instantiate(segmentPrefab);
-        segment.position = segments[segments.Count - 1].position;
+        SnakeSegment segment = Instantiate(segmentPrefab);
+        segment.Follow(segments[segments.Count - 1], segments.Count, segments.Count + 1);
         segments.Add(segment);
     }
 
     public void ResetState()
     {
-        direction = Vector2.right;
-        transform.position = Vector3.zero;
+        // Set the initial direction of the snake, starting at the origin
+        // (center of the grid)
+        head.SetDirection(Vector2.right, Vector2.zero);
+        head.transform.position = Vector3.zero;
 
         // Start at 1 to skip destroying the head
         for (int i = 1; i < segments.Count; i++) {
             Destroy(segments[i].gameObject);
         }
 
-        // Clear the list but add back this as the head
+        // Clear the list then add the head as the first segment
         segments.Clear();
-        segments.Add(transform);
+        segments.Add(head);
 
         // -1 since the head is already in the list
         for (int i = 0; i < initialSize - 1; i++) {